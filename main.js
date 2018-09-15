cc934112fef2a67e9fad297f67db9ef0964d7b5f
Deepak Bharti <deepakbharti823@gmail.com>
2018-09-15 10:06:42 +0530

    initializing main js and main css files

---
+++ main.js
@@ -0,0 +1,11 @@
+    <script>
+
+    // $('.welcomeback').typeIt({ 
+    //   speed: 90, 
+    //   autoStart: false,
+    //   loop : true,
+    //   loopDelay: 1550,
+    // });
+
+
+    </script>
